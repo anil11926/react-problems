26643200f6ab5e4477b5657dc11d07980cca99ea
anil11926 <anilkumar955321336@gmail.com>
2026-06-30 19:55:19 +0530

    First commit

---
+++ my-food-app/src/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <>
+      <p>the first react program has been created</p>
+    </>
+  )
+}
+
+export default Body
---
+++ my-food-app/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function Header() {
+  return (
+    <>
+     <h1>welcome to react program</h1> 
+     <p>this is my react program </p>
+     <h2></h2>
+
+    </>
+  )
+}
+
+export default Header
