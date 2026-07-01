c2985c07e4b383a704b4a04d30021d46d1d11293
anil11926 <anilkumar955321336@gmail.com>
2026-07-01 21:57:57 +0530

    First commit

--- food-app/src/App.tsx
+++ food-app/src/App.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import Home from './home'
 import Veg from './veg'
 import NonVeg from './nonveg'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Nonveg from './nonveg'
 
 function App() {
   return (
     <>
-      <Home />
-      <Veg />
-      <NonVeg />
+      <BrowserRouter>
+        <Link to="/Home">Home</Link>
+        <Link to="/Veg">Veg</Link>
+        <Link to="/NonVeg">Non Veg</Link>
+        <Routes>
+          <Route path="/Home" element={<Home></Home>}></Route>
+          <Route path="/Veg" element={<Veg></Veg>}></Route>
+          <Route path="/NonVeg" element={<Nonveg></Nonveg>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
