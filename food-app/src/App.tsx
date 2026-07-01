41c2ff6b253942b403c5fed5ef4658527f3fe41f
anil11926 <anilkumar955321336@gmail.com>
2026-07-01 20:17:36 +0530

    First commit

---
+++ food-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './home'
+import Veg from './veg'
+import NonVeg from './nonveg'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Veg />
+      <NonVeg />
+    </>
+  )
+}
+
+export default App
---
+++ food-app/src/Nonveg.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NonVeg() {
+    
+  return (
+    <>
+      <h1>This is the non-vegetarian page</h1>
+    </>
+  )
+}
+
+export default NonVeg
